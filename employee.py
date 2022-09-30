3af4ac0110ffa195aa5c7ff130787b26808f6d8c
Saskia Lablans <k21009051@kcl.ac.uk>
2022-09-30 15:37:34 +0100

    Make more efficient

--- employee.py
+++ employee.py
@@ -1,107 +1,98 @@
 """Employee pay calculator."""
 
 class Employee:
-    def __init__(self,name,contract_type,commission_type):
+    def __init__(self,name,contract,commission):
         self.name = name
-        self.contract_type = contract_type
-        self.commission_type = commission_type
-
-        self.hours_worked = 0
-        self.hourly_rate = 0
-        
-        self.monthly_salary = 0
-
-        self.number_of_contracts = 0
-        self.contract_rate = 0
-        
-        self.bonus_commission = 0
-
-        self.contract_pay = 0
-        self.commission_pay = 0
-
-        self.contract_name = ''
-        self.commission_name = ''
-
-    def get_contract_pay(self):
-        if self.contract_type == 'Monthly':
-            return self.monthly_salary
-        elif self.contract_type == 'Hourly':
-            return self.hours_worked * self.hourly_rate
-        else:
-            return 0
+        self.contract = contract
+        self.commission = commission
 
     def get_pay(self):
-        self.contract_pay = self.get_contract_pay()
-        self.commission_pay = self.get_commission_pay() 
-
-        return self.contract_pay + self.commission_pay
+        return self.contract.ans + self.commission.ans
 
     def __str__(self):
-        if self.contract_type == 'Monthly':
-            self.contract_name = f'monthly salary of {self.monthly_salary}'
-        elif self.contract_type == 'Hourly':
-            self.contract_name = f'contract of {self.hours_worked} hours at {self.hourly_rate}/hour'
-        else:
-            self.contract_name == ''
-
-        if self.commission_type == 'Bonus':
-            self.commission_name = f' and receives a bonus commission of {self.bonus_commission}'
-        elif self.commission_type == 'Contract':
-            self.commission_name = f' and receives a commission for {self.number_of_contracts} contract(s) at {self.contract_rate}/contract'
-        else:
-            self.commission_name = ''
-
-        return f'{self.name} works on a {self.contract_name}{self.commission_name}.  Their total pay is {self.get_pay()}.'
+        return f'{self.name} works on a {self.contract.get_contract_string()}{self.commission.get_commission_string()}.  Their total pay is {self.get_pay()}.'
 
 
-    def get_commission_pay(self):
-        if self.commission_type == 'Bonus':
-            return self.bonus_commission
-        elif self.commission_type == 'Contract':
-            return self.number_of_contracts * self.contract_rate
-        else:
-            return 0
+#QUESTION: Is there a more efficient way of simulating multiple constructors?? 
+class Commission():
+    def __init__(self,*args):
+        if len(args) == 2:
+            self.ans = self.calculate_contract_pay(args)
+            self.commission_string = f' and receives a commission for {args[0]} contract(s) at {args[1]}/contract'
+        elif len(args) == 1:
+            self.ans = self.calculate_bonus_pay(args)
+            if args[0] != 0:
+                self.commission_string = f' and receives a bonus commission of {args[0]}'
+            else:
+                self.commission_string = ''
+
+    def calculate_contract_pay(self,args):
+        return args[0] * args[1]
+
+    def calculate_bonus_pay(self,args):
+        return args[0]
+
+    def get_commission_string(self):
+        return self.commission_string
+
+
+class Contract():
+    def __init__(self,*args):
+        if len(args) == 2:
+            self.ans = self.calculate_hourly_pay(args)
+            self.contract_string = f'contract of {args[0]} hours at {args[1]}/hour'
+        elif len(args) == 1:
+            self.ans = self.calculate_monthly_pay(args)
+            self.contract_string = f'monthly salary of {args[0]}'
+
+    def calculate_monthly_pay(self,args):
+        return args[0]
+
+    def calculate_hourly_pay(self,args):
+        return args[0] * args[1]
+
+    def get_contract_string(self):
+        return self.contract_string
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie','Monthly',None)
-
-billie.monthly_salary = 4000
+billie_contract = Contract(4000)
+billie_commission = Commission(0)
+billie = Employee('Billie',billie_contract,billie_commission)
+print(billie.get_pay())
+print(billie.__str__())
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie','Hourly',None)
-
-charlie.hours_worked = 100
-charlie.hourly_rate = 25
+charlie_contract = Contract(100,25)
+charlie_commission = Commission(0)
+charlie = Employee('Charlie',charlie_contract,charlie_commission)
+print(charlie.get_pay())
+print(charlie.__str__())
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee','Monthly','Contract')
-
-renee.monthly_salary = 3000
-
-renee.number_of_contracts = 4
-renee.contract_rate = 200
+renee_contract = Contract(3000)
+renee_commission = Commission(4,200)
+renee = Employee('Renee',renee_contract,renee_commission)
+print(renee.get_pay())
+print(renee.__str__())
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan','Hourly','Contract')
-
-jan.hours_worked = 150
-jan.hourly_rate = 25
-
-jan.number_of_contracts = 3
-jan.contract_rate = 220
+jan_contract = Contract(150,25)
+jan_commission = Commission(3,220)
+jan = Employee('Jan',jan_contract,jan_commission)
+print(jan.get_pay())
+print(jan.__str__())
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie','Monthly','Bonus')
-
-robbie.monthly_salary = 2000
-
-robbie.bonus_commission = 1500 
+robbie_contract = Contract(2000)
+robbie_commission = Commission(1500)
+robbie = Employee('Robbie',robbie_contract,robbie_commission)
+print(robbie.get_pay())
+print(robbie.__str__())
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel','Hourly','Bonus')
-
-ariel.hours_worked = 120
-ariel.hourly_rate = 30
-
-ariel.bonus_commission = 600
+ariel_contract = Contract(120,30)
+ariel_commission = Commission(600)
+ariel = Employee('Ariel',ariel_contract,ariel_commission)
+print(ariel.get_pay())
+print(ariel.__str__())
